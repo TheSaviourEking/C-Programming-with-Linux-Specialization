848063586e3df03d02e541a3736287461dd4040b
Saviour Eking <106761645+TheSaviourEking@users.noreply.github.com>
2022-11-14 03:57:01 +0000

    add week2 tasks solutions

---
+++ C-Programming:-Getting-Started-1/Week2/10. Structure-of-a-simple-c-program.c
@@ -0,0 +1,20 @@
+// preprocessor directive
+#include <stdio.h>
+
+//  main function 
+int main(void){
+    
+    // variable Declaration
+    int i;
+    printf("I ");
+    
+    //  executable statement
+    for(i = 0; i < 3 ; i++) {
+        printf("love ");
+    }
+    
+     printf("programming!");
+     
+    // return statement
+    return(0);
+}
