a27fbf19dee309893cb087b03bd5650598bdf123
Saviour Eking <106761645+TheSaviourEking@users.noreply.github.com>
2022-11-16 04:41:55 +0000

    add 1. Declare-assign-and-print-characters-with-the-%c-format-specifier.c

---
+++ C-Programming:-Getting-Started-1/Week4/1. Declare-assign-and-print-characters-with-the-%c-format-specifier.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    char letter1;
+    char letter2;
+    char letter3;
+   
+    
+    letter1 = 'i';
+    letter2 = 'n';
+    letter3 = 'c';
+    
+    
+   printf ("Programming %c%c %c\n", letter1, letter2, letter3);
+    return 0;
+}
